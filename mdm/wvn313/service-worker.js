113bb0d9c64b9cb43063c133f5b9f0cf8f46e97c
phuongnguyenhnm <phuongnguyenhnm@gmail.com>
2024-10-14 09:52:04 +0700

    Update service-worker.js

--- mdm/wvn313/service-worker.js
+++ mdm/wvn313/service-worker.js
@@ -1,11 +1,25 @@
+const CACHE_NAME = 'local-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.png',
+];
+
+// Caching assets khi Service Worker được cài đặt
 self.addEventListener('install', (event) => {
-    console.log('Service worker installing...');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('activate', (event) => {
-    console.log('Service worker activating...');
-});
-
+// Fetching từ cache khi không có kết nối
 self.addEventListener('fetch', (event) => {
-    console.log('Fetching:', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
