e6000f7a4a17303cdc94c90b6aa2c92c0e87e6da
phuongnguyenhnm <phuongnguyenhnm@gmail.com>
2024-10-15 13:34:12 +0700

    Update service-worker.js

--- mdm/wvn313/service-worker.js
+++ mdm/wvn313/service-worker.js
@@ -4,6 +4,18 @@ const urlsToCache = [
   '/index.html',
   '/manifest.json',
   '/icon.png',
+  '/app.js',
+  '/bg.png',
+  '/RobotoFlex-VariableFont_GRAD,XOPQ,XTRA,YOPQ,YTAS,YTDE,YTFI,YTLC,YTUC,opsz,slnt,wdth,wght.ttf',
+  '/camera.6af3398a7aff653bb9dbb034a57a4111.png',
+  '/download.svg',
+  '/jquery.min.js',
+  '/jquery.qrcode.min.js',
+  '/name.txt',
+  '/password.f36456584862850105cf3b32f8086137.png',
+  '/service-worker.js',
+  '/styles.css',
+  '/username.1a1e028fff62438b9e32f10d4aeff6b8.png',
 ];
 
 // Caching assets khi Service Worker được cài đặt
